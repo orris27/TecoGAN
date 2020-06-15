2d49e7b968c6061fad45185d1e4542ac8438304a
orris27 <gsorris27@gmail.com>
2020-06-15 16:53:25 +0800

    random

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ import random as rn
 os.environ['PYTHONHASHSEED'] = '0'
 np.random.seed(42)
 rn.seed(12345)
-tf.set_random_seed(1234)
+#tf.set_random_seed(1234)
+tf.random.set_seed(1234)
 
 import tensorflow.contrib.slim as slim
 import sys, shutil, subprocess
